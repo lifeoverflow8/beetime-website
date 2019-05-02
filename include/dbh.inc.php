5b1815ac0278e552b9803c10a9911c83fa358464
lifeoverflow <lifeoverflow8@gmail.com>
2019-05-02 09:53:50 +0800

    make website not crash when it can't connect to database.
    yep

--- include/dbh.inc.php
+++ include/dbh.inc.php
@@ -7,7 +7,7 @@
     
     $conn = mysqli_connect($servername, $username, $password, $database, $dbport);
     
-    if (!$conn) {
+    /*if (!$conn) {
         die("Connection failed: ". mysqli_connect_error());
-    }
+    }*/
 ?>
